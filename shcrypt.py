defd517f410ec1a476f1b02b4dde073c865897ce
joknarf <joknarf@free.fr>
2023-02-08 22:49:49 +0100

    use sys.stdout.write + detect tty

--- shcrypt.py
+++ shcrypt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# pylint: disable=C0301,R0913,R0914
+""" openssl encrypt to autodecrypt shell/raw """
 import os
 import sys
 import argparse
@@ -7,10 +9,11 @@ from textwrap import dedent
 from uuid import uuid4
 from getpass import getpass
 
-ossl = 'openssl enc -aes256 -md sha512 -a'
+OSSL = 'openssl enc -aes256 -md sha512 -a'
 
 def crypt(data, password=None):
-    osslenc = f'{ossl} -salt'
+    """ crypt data """
+    osslenc = f'{OSSL} -salt'
     if password:
         osslenc += f" -pass fd:3 3<<<'{password}'"
     runsh = run(osslenc, input=data, stdout=PIPE, shell=True,
@@ -18,27 +21,31 @@ def crypt(data, password=None):
     if runsh.returncode != 0:
         print('Error: Failed to encrypt', file=sys.stderr)
         return False
-    return runsh.stdout[:-1] if runsh.stdout.endswith('\n') else runsh.stdout
+    return runsh.stdout
 
-def decrypt(data, password=None):    
+def decrypt(data, password=None):
+    """ decrypt data """
     password = password or getpass('🔐 Password: ')
-    runsh = run(f"{ossl} -d -pass fd:3 3<<<'{password}'", shell=True, input=data, stdout=PIPE,
+    runsh = run(f"{OSSL} -d -pass fd:3 3<<<'{password}'", shell=True, input=data, stdout=PIPE,
                 stderr=DEVNULL, encoding='utf-8', check=False, executable='/bin/bash')
     if runsh.returncode != 0:
         print('Error: Failed to decrypt', file=sys.stderr)
         return False
-    return runsh.stdout[:-1] if runsh.stdout.endswith('\n') else runsh.stdout
+    return runsh.stdout
 
 def sshsign(sshkey=None, signtext='constant_sign'):
+    """ ssh signature using sshkey """
     sshkey = sshkey or os.path.expanduser('~') + '/.ssh/id_rsa'
-    runsh = run(f"ssh-keygen -Y sign -f '{sshkey}' -n file - <<<'{signtext}'", stdout=PIPE, 
+    runsh = run(f"ssh-keygen -Y sign -f '{sshkey}' -n file - <<<'{signtext}'", stdout=PIPE,
                 stderr=DEVNULL, encoding='utf-8', check=False, shell=True, executable='/bin/bash')
     if runsh.returncode != 0:
         print('Error: Failed to get ssh signature (openssl version ?)', file=sys.stderr)
         return False
     return ''.join(runsh.stdout.strip().split('\n')[1:])
 
-def cryptas(data, mode='shellout', pwmode='passwd', passvar=None, varname=None, sshkey=None, password=None):
+def cryptas(data, mode='shellout', pwmode='passwd', passvar=None,
+            varname=None, sshkey=None, password=None):
+    """ crypt data to shell auto-decrypt """
     sshkey = sshkey or os.path.expanduser('~') + '/.ssh/id_rsa'
     passvar = passvar or uuid4().hex
     sshkeyfind = f"$([ -f '{sshkey}' ] && echo '{sshkey}' || echo '<(ssh-add -L 2>/dev/null|head -n 1)')"
@@ -52,17 +59,17 @@ def cryptas(data, mode='shellout', pwmode='passwd', passvar=None, varname=None, 
     pwmodes = {
         'passwd': {'bashpass':'', 'osslpass': '', 'unset': ':' },
         'pwcache': {
-            'bashpass': bashpass, 
+            'bashpass': bashpass,
             'osslpass': f'-pass fd:3 3<<<$(base64 -d <<<"${{{passvar}}}")',
             'unset': f'unset {passvar}',
         },
         'pwcache2': {
-            'bashpass': bashpass, 
+            'bashpass': bashpass,
             'osslpass': f'-pass pass:$(base64 -d <<<"${{{passvar}}}")',
             'unset': f'unset {passvar}',
         },
         'sshsign': {
-            'bashpass': '', 
+            'bashpass': '',
             'osslpass': f"-pass fd:3 3<<<{signwithkey}",
             'unset': ':',
         }
@@ -91,8 +98,8 @@ def cryptas(data, mode='shellout', pwmode='passwd', passvar=None, varname=None, 
         {preossl}{ossl} -d {osslpass} <<'EOZ' 2>/dev/null {postossl}
         {crypted}
         EOZ
-        {postcrypt}\
-    """).format(ossl=ossl, crypted=crypted, **mod, **pwm)
+        {postcrypt}
+    """).format(ossl=OSSL, crypted=crypted, **mod, **pwm)
     return shell
 
 
@@ -108,28 +115,26 @@ if __name__ == '__main__':
     parser.add_argument("-d", "--decrypt", default=False, action='store_true', help='decrypt raw')
     parser.add_argument("-i", "--interactive", default=False, action='store_true', help='Get secret from console')
     args = parser.parse_args()
-    
-    if args.interactive:
+
+    if sys.stdin.isatty() and args.var :
         indata = getpass('🔐 Secret: ')
     else:
         indata = sys.stdin.read()
-        if indata.endswith('\n'):
-            indata = indata[:-1]
-    
+
     if args.var:
         args.mode = 'shellvar'
 
     if args.decrypt:
         if args.pwmode == 'sshsign':
-            print(decrypt(indata, sshsign(args.key)))
+            sys.stdout.write(decrypt(indata, sshsign(args.key)))
         else:
-            print(decrypt(indata))
+            sys.stdout.write(decrypt(indata))
         sys.exit(0)
 
     if args.mode == 'raw':
         if args.pwmode == 'sshsign':
-            print(crypt(indata, sshsign(args.key)))
+            sys.stdout.write(crypt(indata, sshsign(args.key)))
         else:
-            print(crypt(indata))
+            sys.stdout.write(crypt(indata))
         sys.exit(0)
-    print(cryptas(indata, args.mode, args.pwmode, args.cachevar, args.var, args.key))
+    sys.stdout.write(cryptas(indata, args.mode, args.pwmode, args.cachevar, args.var, args.key))
